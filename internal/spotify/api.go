ce3736cf83b098d72fc47323558c30b13bc411c1
rcong315 <richardcong635@gmail.com>
2025-03-06 23:33:55 -0800

    Get track data

--- internal/spotify/api.go
+++ internal/spotify/api.go
@@ -5,187 +5,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 const spotifyAPIURL = "https://api.spotify.com/v1/"
 const limitMax = 50
 
-func fetchPaginatedItems(token string, apiURL string, limit int, offset int, responseType interface{}) []string {
-	var ids []string
-
-	url := fmt.Sprintf("%s%s?limit=%d&offset=%d", spotifyAPIURL, apiURL, limit, offset)
-
-	for {
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			log.Printf("Error creating request: %v", err)
-			return ids
-		}
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Printf("Error making GET request: %v", err)
-			return ids
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			log.Printf("Error from Spotify server: %d for URL: %s", resp.StatusCode, url)
-			if resp.StatusCode == http.StatusTooManyRequests {
-				log.Printf("Rate limited, waiting 1 minute")
-				time.Sleep(60 * time.Second)
-				continue
-			}
-			return ids
-		}
-
-		if err := json.NewDecoder(resp.Body).Decode(responseType); err != nil {
-			log.Printf("Error decoding response: %v", err)
-			return ids
-		}
-
-		var nextURL string
-		var itemCount int
-
-		switch response := responseType.(type) {
-		case *UsersPlaylistsResponse:
-			for _, item := range response.Items {
-				ids = append(ids, item.Id)
-			}
-			itemCount = len(response.Items)
-			nextURL = response.Next
-		case *UsersTopArtistsResponse:
-			for _, item := range response.Items {
-				ids = append(ids, item.Id)
-			}
-			itemCount = len(response.Items)
-			nextURL = response.Next
-		case *ArtistsAlbumsResponse:
-			for _, item := range response.Items {
-				ids = append(ids, item.Id)
-			}
-			itemCount = len(response.Items)
-			nextURL = response.Next
-		case *PlaylistsTracksResponse:
-			for _, item := range response.Items {
-				if item.Track.Id != "" {
-					ids = append(ids, item.Track.Id)
-				}
-			}
-			itemCount = len(response.Items)
-			nextURL = response.Next
-		case *UsersTopTracksResponse:
-			for _, item := range response.Items {
-				ids = append(ids, item.Id)
-			}
-			itemCount = len(response.Items)
-			nextURL = response.Next
-		case *UsersSavedTracksResponse:
-			for _, item := range response.Items {
-				if item.Track.Id != "" {
-					ids = append(ids, item.Track.Id)
-				}
-			}
-			itemCount = len(response.Items)
-			nextURL = response.Next
-		case *AlbumsTracksResponse:
-			for _, item := range response.Items {
-				ids = append(ids, item.Id)
-			}
-			itemCount = len(response.Items)
-			nextURL = response.Next
-		case *UsersFollowedArtists:
-			for _, item := range response.Artists.Items {
-				ids = append(ids, item.Id)
-			}
-			itemCount = len(response.Artists.Items)
-			nextURL = response.Artists.Next
-		case *ArtistsTopTrackResponse:
-			for _, track := range response.Tracks {
-				ids = append(ids, track.Id)
-			}
-			itemCount = len(response.Tracks)
-		default:
-			log.Printf("Unknown response type: %T", responseType)
-			return ids
-		}
-
-		if itemCount == 0 || nextURL == "" {
-			return ids
-		}
-
-		url = nextURL
+func getNextURL(response interface{}) string {
+	switch r := response.(type) {
+	case *UsersTopTracksResponse:
+		return r.Next
+	case *UsersSavedTracksResponse:
+		return r.Next
+	case *UsersPlaylistsResponse:
+		return r.Next
+	case *PlaylistsTracksResponse:
+		return r.Next
+	case *UsersTopArtistsResponse:
+		return r.Next
+	case *UsersFollowedArtists:
+		return r.Artists.Next
+	case *ArtistsTopTrackResponse:
+		return "" // No pagination for top tracks
+	case *ArtistsAlbumsResponse:
+		return r.Next
+	case *AlbumsTracksResponse:
+		return r.Next
+	default:
+		return ""
 	}
 }
 
-func getUsersTopTracks(token string) []string {
-	apiURL := "me/top/tracks"
-	var response UsersTopTracksResponse
-	return fetchPaginatedItems(token, apiURL, limitMax, 0, &response)
-}
-
-func getUsersSavedTracks(token string) []string {
-	apiURL := "me/tracks"
-	var response UsersSavedTracksResponse
-	return fetchPaginatedItems(token, apiURL, limitMax, 0, &response)
-}
-
-func getUsersPlaylists(token string) []string {
-	apiURL := "me/playlists"
-	var response UsersPlaylistsResponse
-	return fetchPaginatedItems(token, apiURL, limitMax, 0, &response)
-}
-
-func getPlaylistsTracks(token, id string) []string {
-	apiURL := fmt.Sprintf("playlists/%s/tracks", id)
-	var response PlaylistsTracksResponse
-	return fetchPaginatedItems(token, apiURL, limitMax, 0, &response)
-}
-
-func getUsersTopArtists(token string) []string {
-	apiURL := "me/top/artists?"
-	var response UsersTopArtistsResponse
-	return fetchPaginatedItems(token, apiURL, limitMax, 0, &response)
-}
-
-func getUsersFollowedArtists(token string) []string {
-	apiURL := "me/following?type=artist"
-	var response UsersFollowedArtists
-	return fetchPaginatedItems(token, apiURL, limitMax, 0, &response)
-}
-
-func getArtistsTopTracks(token, id string) []string {
-	apiURL := fmt.Sprintf("artists/%s/top-tracks", id)
-	var response ArtistsTopTrackResponse
-	return fetchPaginatedItems(token, apiURL, limitMax, 0, &response)
-}
-
-func getArtistsAlbums(token, id string) []string {
-	apiURL := fmt.Sprintf("artists/%s/albums", id)
-	var response ArtistsAlbumsResponse
-	return fetchPaginatedItems(token, apiURL, limitMax, 0, &response)
-}
-
-func getAlbumsTracks(token, id string) []string {
-	apiURL := fmt.Sprintf("albums/%s/tracks", id)
-	var response AlbumsTracksResponse
-	return fetchPaginatedItems(token, apiURL, limitMax, 0, &response)
-}
-
-func GetAudioFeatures(token string, ids []string) []AudioFeaturesResponse {
-	var audioFeatures []AudioFeaturesResponse
-	var url = fmt.Sprintf("%s/audio-features?ids=%s", spotifyAPIURL, ids[0])
-	for i := 1; i < len(ids); i++ {
-		url = fmt.Sprintf("%s,%s", url, ids[i])
-	}
-
+func fetchPaginatedItems(token string, url string, responseType interface{}) (interface{}, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Printf("Error creating request: %v", err)
-		return audioFeatures
+		return nil, err
 	}
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 
@@ -193,22 +49,170 @@ func GetAudioFeatures(token string, ids []string) []AudioFeaturesResponse {
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Printf("Error making GET request: %v", err)
-		return audioFeatures
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		log.Printf("Error from Spotify server: %d for URL: %s", resp.StatusCode, url)
-		return audioFeatures
+		if resp.StatusCode == http.StatusTooManyRequests {
+			retryAfter := 60
+			if retryHeader := resp.Header.Get("Retry-After"); retryHeader != "" {
+				if seconds, err := strconv.Atoi(retryHeader); err == nil {
+					retryAfter = seconds
+				}
+			}
+			log.Printf("Rate limited, waiting %d seconds", retryAfter)
+			time.Sleep(time.Duration(retryAfter) * time.Second)
+		}
+		return nil, fmt.Errorf("server returned status code %d", resp.StatusCode)
 	}
 
-	var response struct {
-		AudioFeatures []AudioFeaturesResponse `json:"audio_features"`
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(responseType); err != nil {
 		log.Printf("Error decoding response: %v", err)
-		return audioFeatures
+		return nil, err
+	}
+	return responseType, nil
+}
+
+func fetchAllResults(token string, url string, responseType interface{}) ([]interface{}, error) {
+	var results []interface{}
+	for {
+		response, err := fetchPaginatedItems(token, url, responseType)
+		if err != nil {
+			return results, err
+		}
+		results = append(results, response)
+
+		nextURL := getNextURL(response)
+		if nextURL == "" {
+			break
+		}
+		url = nextURL
+	}
+	return results, nil
+}
+
+func getUsersTopTracks(token string) ([]Track, error) {
+	var allTracks []Track
+	url := fmt.Sprintf("%sme/top/tracks/?limit=%d&offset=%d", spotifyAPIURL, limitMax, 0)
+	responseType := &UsersTopTracksResponse{}
+
+	responses, err := fetchAllResults(token, url, responseType)
+	if err != nil {
+		return nil, err
+	}
+	for _, response := range responses {
+		if typedResponse, ok := response.(*UsersTopTracksResponse); ok {
+			allTracks = append(allTracks, typedResponse.Items...)
+		}
 	}
 
-	return response.AudioFeatures
+	return allTracks, nil
 }
+
+// func getUsersSavedTracks(token string) ([]UsersSavedTracksResponse, error) {
+// 	var response []UsersSavedTracksResponse
+// 	if err := fetchPaginatedItems(token, "me/tracks", limitMax, 0, &response); err != nil {
+// 		return nil, err
+// 	}
+// 	return response, nil
+// }
+
+// func getUsersPlaylists(token string) ([]UsersPlaylistsResponse, error) {
+// 	var response []UsersPlaylistsResponse
+// 	if err := fetchPaginatedItems(token, "me/playlists", limitMax, 0, &response); err != nil {
+// 		return nil, err
+// 	}
+// 	return response, nil
+// }
+
+// func getPlaylistsTracks(token, id string) ([]PlaylistsTracksResponse, error) {
+// 	var response []PlaylistsTracksResponse
+// 	apiURL := fmt.Sprintf("playlists/%s/tracks", id)
+// 	if err := fetchPaginatedItems(token, apiURL, limitMax, 0, &response); err != nil {
+// 		return nil, err
+// 	}
+// 	return response, nil
+// }
+
+// func getUsersTopArtists(token string) ([]UsersTopArtistsResponse, error) {
+// 	var response []UsersTopArtistsResponse
+// 	if err := fetchPaginatedItems(token, "me/top/artists", limitMax, 0, &response); err != nil {
+// 		return nil, err
+// 	}
+// 	return response, nil
+// }
+
+// func getUsersFollowedArtists(token string) ([]UsersFollowedArtists, error) {
+// 	var response []UsersFollowedArtists
+// 	if err := fetchPaginatedItems(token, "me/following?type=artist", limitMax, 0, &response); err != nil {
+// 		return nil, err
+// 	}
+// 	return response, nil
+// }
+
+// func getArtistsTopTracks(token, id string) ([]ArtistsTopTrackResponse, error) {
+// 	var response []ArtistsTopTrackResponse
+// 	apiURL := fmt.Sprintf("artists/%s/top-tracks", id)
+// 	if err := fetchPaginatedItems(token, apiURL, limitMax, 0, &response); err != nil {
+// 		return nil, err
+// 	}
+// 	return response, nil
+// }
+
+// func getArtistsAlbums(token, id string) ([]ArtistsAlbumsResponse, error) {
+// 	var response []ArtistsAlbumsResponse
+// 	apiURL := fmt.Sprintf("artists/%s/albums", id)
+// 	if err := fetchPaginatedItems(token, apiURL, limitMax, 0, &response); err != nil {
+// 		return nil, err
+// 	}
+// 	return response, nil
+// }
+
+// func getAlbumsTracks(token, id string) ([]AlbumsTracksResponse, error) {
+// 	var response []AlbumsTracksResponse
+// 	apiURL := fmt.Sprintf("albums/%s/tracks", id)
+// 	if err := fetchPaginatedItems(token, apiURL, limitMax, 0, &response); err != nil {
+// 		return nil, err
+// 	}
+// 	return response, nil
+// }
+
+// func GetAudioFeatures(token string, ids []string) []AudioFeaturesResponse {
+// 	var audioFeatures []AudioFeaturesResponse
+// 	var url = fmt.Sprintf("%s/audio-features?ids=%s", spotifyAPIURL, ids[0])
+// 	for i := 1; i < len(ids); i++ {
+// 		url = fmt.Sprintf("%s,%s", url, ids[i])
+// 	}
+
+// 	req, err := http.NewRequest("GET", url, nil)
+// 	if err != nil {
+// 		log.Printf("Error creating request: %v", err)
+// 		return audioFeatures
+// 	}
+// 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+// 	client := &http.Client{}
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		log.Printf("Error making GET request: %v", err)
+// 		return audioFeatures
+// 	}
+// 	defer resp.Body.Close()
+
+// 	if resp.StatusCode != http.StatusOK {
+// 		log.Printf("Error from Spotify server: %d for URL: %s", resp.StatusCode, url)
+// 		return audioFeatures
+// 	}
+
+// 	var response struct {
+// 		AudioFeatures []AudioFeaturesResponse `json:"audio_features"`
+// 	}
+// 	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+// 		log.Printf("Error decoding response: %v", err)
+// 		return audioFeatures
+// 	}
+
+// 	return response.AudioFeatures
+// }
--- internal/spotify/getAllTracks.go
+++ internal/spotify/getAllTracks.go
@@ -1,71 +1,83 @@
 package spotify
 
-import "log"
+import (
+	"encoding/json"
+	"log"
+)
 
 func GetAllTracks(token string) ([]Track, []Album, []Artist, []Playlist) {
-	var idsSet = make(map[string]struct{})
-	var trackCount = 0
+	// var idsSet = make(map[string]struct{})
+	// var trackCount = 0
 
-	addTrack := func(id string) {
-		if _, exists := idsSet[id]; !exists {
-			idsSet[id] = struct{}{}
-			trackCount++
-			if trackCount%500 == 0 {
-				log.Printf("Added %d tracks, latest: %s", trackCount, id)
-			}
-		}
-	}
+	// addTrack := func(id string) {
+	// 	if _, exists := idsSet[id]; !exists {
+	// 		idsSet[id] = struct{}{}
+	// 		trackCount++
+	// 		if trackCount%500 == 0 {
+	// 			log.Printf("Added %d tracks, latest: %s", trackCount, id)
+	// 		}
+	// 	}
+	// }
 
 	// User's top tracks
 	log.Print("Getting user's top tracks")
-	for _, id := range getUsersTopTracks(token) {
-		addTrack(id)
+	usersTopTracks, err := getUsersTopTracks(token)
+	if err != nil {
+		log.Printf("Error getting user's top tracks: %v", err)
 	}
-
-	// User's saved tracks
-	log.Printf("Getting user's saved tracks")
-	for _, id := range getUsersSavedTracks(token) {
-		addTrack(id)
-	}
-
-	// User's playlists
-	log.Printf("Getting tracks from user's playlists")
-	var playlistIds = getUsersPlaylists(token)
-	for _, playlistId := range playlistIds {
-		for _, id := range getPlaylistsTracks(token, playlistId) {
-			addTrack(id)
+	for _, track := range usersTopTracks {
+		trackJSON, err := json.Marshal(track)
+		if err != nil {
+			log.Printf("Error marshaling track: %v", err)
+			continue
 		}
+		log.Printf(string(trackJSON))
 	}
 
-	// User's top artists and followed artists
-	log.Printf("Getting tracks from user's top and followed artists")
-	var topArtistsIds = getUsersTopArtists(token)
-	var followedArtistIds = getUsersFollowedArtists(token)
-	artistsMap := make(map[string]struct{})
-	for _, id := range topArtistsIds {
-		artistsMap[id] = struct{}{}
-	}
-	for _, id := range followedArtistIds {
-		artistsMap[id] = struct{}{}
-	}
-	var albumIds []string
-	for _, artistId := range artistsMap {
-		for _, traickId := range getArtistsTopTracks(token, artistId) {
-			addTrack(traickId)
-		}
-		albumIds = append(albumIds, getArtistsAlbums(token, artistId)...)
-	}
-	for _, id := range albumIds {
-		for _, id := range getAlbumsTracks(token, id) {
-			idsSet[id] = struct{}{}
-			log.Printf("Added track Id from album: %s", id)
-		}
-	}
+	// // User's saved tracks
+	// log.Printf("Getting user's saved tracks")
+	// for _, id := range getUsersSavedTracks(token) {
+	// 	addTrack(id)
+	// }
 
-	var ids []string
-	for id := range idsSet {
-		ids = append(ids, id)
-	}
+	// // User's playlists
+	// log.Printf("Getting tracks from user's playlists")
+	// var playlistIds = getUsersPlaylists(token)
+	// for _, playlistId := range playlistIds {
+	// 	for _, id := range getPlaylistsTracks(token, playlistId) {
+	// 		addTrack(id)
+	// 	}
+	// }
+
+	// // User's top artists and followed artists
+	// log.Printf("Getting tracks from user's top and followed artists")
+	// var topArtistsIds = getUsersTopArtists(token)
+	// var followedArtistIds = getUsersFollowedArtists(token)
+	// artistsMap := make(map[string]struct{})
+	// for _, id := range topArtistsIds {
+	// 	artistsMap[id] = struct{}{}
+	// }
+	// for _, id := range followedArtistIds {
+	// 	artistsMap[id] = struct{}{}
+	// }
+	// var albumIds []string
+	// for _, artistId := range artistsMap {
+	// 	for _, traickId := range getArtistsTopTracks(token, artistId) {
+	// 		addTrack(traickId)
+	// 	}
+	// 	albumIds = append(albumIds, getArtistsAlbums(token, artistId)...)
+	// }
+	// for _, id := range albumIds {
+	// 	for _, id := range getAlbumsTracks(token, id) {
+	// 		idsSet[id] = struct{}{}
+	// 		log.Printf("Added track Id from album: %s", id)
+	// 	}
+	// }
+
+	// var ids []string
+	// for id := range idsSet {
+	// 	ids = append(ids, id)
+	// }
 
 	return ids
 }
