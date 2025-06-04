5013e164142fc5dcc123736725daac2a181ff241
rcong315 <richardcong635@gmail.com>
2025-06-03 21:40:56 -0700

    5 workers

--- internal/service/processItems.go
+++ internal/service/processItems.go
@@ -21,7 +21,7 @@ func processAll(token string, userId string) {
 			zap.String("userId", userId),
 			zap.Time("startTime", startTime))
 
-		numWorkers := 10
+		numWorkers := 5
 		jobQueueSize := 100 * 1000
 
 		pool := NewWorkerPool(numWorkers, jobQueueSize)
