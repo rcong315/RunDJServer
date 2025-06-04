a91e699b5a924970937410d4e220f2b98477ca77
rcong315 <richardcong635@gmail.com>
2025-06-03 21:04:11 -0700

    Increase workers

--- internal/service/processItems.go
+++ internal/service/processItems.go
@@ -21,7 +21,7 @@ func processAll(token string, userId string) {
 			zap.String("userId", userId),
 			zap.Time("startTime", startTime))
 
-		numWorkers := 30
+		numWorkers := 100
 		jobQueueSize := 100 * 1000
 
 		pool := NewWorkerPool(numWorkers, jobQueueSize)
